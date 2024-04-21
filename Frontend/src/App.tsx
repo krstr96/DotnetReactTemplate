99216913f04fbf6f6a3660a5ae0a1ec857fa336a
Keegan Strohm <krstr96@gmail.com>
2024-04-21 14:26:56 -0400

    Move from CRA to Vite

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
-    const [message, setMessage] = useState("Loading...");
+  const [message, setMessage] = useState("Loading...");
 
-    useEffect(() => {
-        fetch("/api/hello-world")
-            .then(response => response.text())
-            .then(text => setMessage(text))
-            .catch(error => setMessage(error));
-    }, []);
+  useEffect(() => {
+    fetch("/api/hello-world")
+      .then(response => response.text())
+      .then(text => setMessage(text))
+      .catch(error => setMessage(error));
+  }, []);
 
-    return (
-        <h1>{message}</h1>
-    );
+  return (
+    <h1>{message}</h1>
+  );
 }
